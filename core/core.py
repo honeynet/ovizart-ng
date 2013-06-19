caf76116a60a3c679e7d2ab367b37ddd1285ce4d
ggercek <gurcangercek@gmail.com>
2013-06-19 16:23:45 +0300

    MissingArgumentException added

--- core/core.py
+++ core/core.py
@@ -132,3 +132,21 @@ class MissingMethodException(Exception):
 
     def __str__(self):
         return "\"%s\" method is missing on class definition \"%s\"" %(self.methodName, self.cls)
+
+
+class MissingArgumentException(Exception):
+    """Exception for missing argument definitions"""
+
+    def __init__(self, argName, methodName, cls):
+        """
+        @param argName      : missing argument name
+        @param methodName   : missing arguments method
+        @param cls          : missing methods class
+        @return
+        """
+        self.argName = argName
+        self.methodName = methodName
+        self.cls = cls
+
+    def __str__(self):
+        return "method %s(%s) is missing on class definition \"%s\"" % (self.methodName, self.argName, self.cls)
