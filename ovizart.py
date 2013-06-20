e31f67dd2a2143cdf5f7a65982c3b7ae30c57f36
ggercek <gurcangercek@gmail.com>
2013-06-20 10:56:58 +0300

    style fix. updateConfig and setConfig methods added

--- ovizart.py
+++ ovizart.py
@@ -10,7 +10,7 @@ from tagger import *
 from analyzer import *
 
 
-class OvizArt():
+class Ovizart():
     """
     Main class to define usage interface
     """
@@ -18,16 +18,26 @@ class OvizArt():
     def __init__(self, config):
         self.config = config
 
+    def updateConfig(self, key, value):
+        """Updates a config property"""
+        pass
+
+    def setConfig(self, config):
+        """Overwrites config"""
+        result = False
+        if config:
+            self.config = config
+            result = True
+        return result
+
     def setInputFile(self, inputFile):
-        """ Sets the input file name
+        """Sets the input file name
         this method is used for local files or after the upload process
         """
         self.inputFile = inputFile
 
     def uploadFile(self, file):
-        """
-        Uploads the remote file to the server then sets the filename
-        """
+        """Uploads the remote file to the server then sets the filename"""
         # TODO: upload file and return the filename. Should be this part handled by built-in web server ?!
         filename = ""
         # filename = upload(file)
@@ -38,33 +48,29 @@ class OvizArt():
         # May be we should return some kind of handler/id for the analyze process
         pass
 
-    def getStatus(self, analyzeId):
-        """
-        Returns status of analyze as RUNNING, CANCELED, FINISHED, ERROR
-        """
-        pass
-
-    def stop(self, analyzeId):
-        """
-        Cancels a RUNNING analyze
+    def stop(self, analysisId):
+        """Cancels a RUNNING analyze
         """
         pass
 
-    def getReport(self, analyzeId):
+    def getStatus(self, analysisId):
+        """Returns status of analyze as RUNNING, CANCELED, FINISHED, ERROR
         """
-        Returns the analyze report
+        pass
+
+    def getReport(self, analysisId):
+        """Returns the analyze report
         """
         pass
 
     def listAvailableModules(self):
-        """
-        Prints available modules in a human readable way.
+        """Prints available modules in a human readable way.
         """
         list_available_modules()
 
 
 if __name__ == '__main__':
     config = {}
-    ovizart = OvizArt(config)
+    ovizart = Ovizart(config)
     ovizart.listAvailableModules()
     
