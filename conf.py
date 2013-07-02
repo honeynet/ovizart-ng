d463ddaf64e5bbc8d6430fa4ad3115d8de651516
ggercek <gurcangercek@gmail.com>
2013-07-02 15:33:13 +0300

    cuckoo options added

--- conf.py
+++ conf.py
@@ -7,4 +7,13 @@ class Config():
         self.exclude_analyzer = []
         self.input_files = []
 
+        # Cuckoo options
+        self.is_cuckoo_available = True
+        self.cuckoo_ip = "localhost"
+        self.cuckoo_port = 9000
+        self.cuckoo_html_enabled = True
+        self.cuckoo_timeout = 60
+        self.cuckoo_tcpdump_enabled = True
+
+
 
