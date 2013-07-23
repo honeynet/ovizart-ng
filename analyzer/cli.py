523b5ced3f1169542392329e73e087673337f9dc
mh <zqzas1@gmail.com>
2013-07-23 17:49:34 +0800

    CLI for wrappers

---
+++ analyzer/cli.py
@@ -0,0 +1,62 @@
+"""
+A CLI for analyzer wrappers
+
+"""
+
+__author__ = "zqzas"
+
+import argparse
+import sys
+sys.path.append("cuckoo")
+sys.path.append("jsunpack-n")
+sys.path.append("virustotal")
+sys.path.append("../")
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description = "An CLI for analyzer wrappers.")
+
+    parser.add_argument("-v", "--virustotal", action = "store_true", help = "Using Virustotal to analyze binary or url")
+    parser.add_argument("-c", "--cuckoo", action = "store_true", help = "Using Cuckoo Sandbox to analyze binary")
+
+    parser.add_argument("-j", "--jsunpackn", action = "store_true", help = "Using Jsunpack-n to analyze url")
+    
+    parser.add_argument("-u" , "--url", help = "Input an url")
+
+    parser.add_argument("-b", "--binary", help = "Input an binary (path)")
+
+    args = parser.parse_args()
+
+    if args.url == None and args.binary == None:
+        print "You must input an url or binary(path)."
+        return
+
+    response = []
+    if args.virustotal:
+        from vt_wrapper import VTWrapper
+        print "Virustotal analyzing", '.' * 30
+        analyzer = VTWrapper()
+        if args.url != None:
+            response += analyzer.analyzeUrl(args.url)
+        else:
+            response += analyzer.analyzeBinary(args.binary)
+
+    if args.cuckoo:
+        from cuckoo_wrapper import CuckooWrapper
+        print "Cuckoo analyzing", '.' * 30
+        analyzer = CuckooWrapper()
+        if args.binary != None:
+            response += str(analyzer.analyzeMalware(args.binary))
+
+
+    if args.jsunpackn:
+        from jsunpackn_wrapper import JsunpacknWrapper
+        print "Jsunpack-n analyzing", '.' * 30
+        analyzer = JsunpacknWrapper()
+
+        if args.url != None:
+            response += analyzer.analyzeJs(args.url)
+
+if __name__ == "__main__":
+    main()
