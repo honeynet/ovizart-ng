7540a95d2aaa3678fb7b49bfa2609b104dbb8aaf
mh <zqzas1@gmail.com>
2013-07-26 22:44:19 +0800

    shell type interface(command dialog) for CLI

--- analyzer/cli.py
+++ analyzer/cli.py
@@ -15,50 +15,59 @@ sys.path.append("../")
 
 
 def main():
-    parser = argparse.ArgumentParser(description = "An CLI for analyzer wrappers.")
+    start = True
 
-    parser.add_argument("-v", "--virustotal", action = "store_true", help = "Using VirusTotal to analyze binary or url")
-    parser.add_argument("-c", "--cuckoo", action = "store_true", help = "Using Cuckoo Sandbox to analyze binary")
+    while True:
+        parser = argparse.ArgumentParser(description = "An CLI for analyzer wrappers.")
 
-    parser.add_argument("-j", "--jsunpackn", action = "store_true", help = "Using Jsunpack-n to analyze url")
-    
-    parser.add_argument("-u" , "--url", help = "Input an url")
+        parser.add_argument("-v", "--virustotal", action = "store_true", help = "Using VirusTotal to analyze binary or url")
+        parser.add_argument("-c", "--cuckoo", action = "store_true", help = "Using Cuckoo Sandbox to analyze binary")
 
-    parser.add_argument("-b", "--binary", help = "Input an binary (path)")
-
-    args = parser.parse_args()
-
-    if args.url == None and args.binary == None:
-        print "You must input an url or binary(path)."
-        print parser.print_help()
-        return
+        parser.add_argument("-j", "--jsunpackn", action = "store_true", help = "Using Jsunpack-n to analyze url")
         
+        parser.add_argument("-u" , "--url", help = "Input an url")
 
-    response = []
-    if args.virustotal:
-        from vt_wrapper import VTWrapper
-        print "Virustotal analyzing", '.' * 30
-        analyzer = VTWrapper()
-        if args.url != None:
-            response += analyzer.analyzeUrl(args.url)
+        parser.add_argument("-b", "--binary", help = "Input an binary (path)")
+
+        if start:
+            args = parser.parse_args()
+            start = False
         else:
-            response += analyzer.analyzeBinary(args.binary)
+            cmd = str(raw_input(">>>"))
+            args = parser.parse_args(cmd.split())
 
-    if args.cuckoo:
-        from cuckoo_wrapper import CuckooWrapper
-        print "Cuckoo analyzing", '.' * 30
-        analyzer = CuckooWrapper()
-        if args.binary != None:
-            response += str(analyzer.analyzeMalware(args.binary))
+        if args.url == None and args.binary == None:
+            print "You must input an url or binary(path)."
+            print parser.print_help()
+            #return
+            continue # continuous shell type interface 
+            
+
+        response = []
+        if args.virustotal:
+            from vt_wrapper import VTWrapper
+            print "Virustotal analyzing", '.' * 30
+            analyzer = VTWrapper()
+            if args.url != None:
+                response += analyzer.analyzeUrl(args.url)
+            else:
+                response += analyzer.analyzeBinary(args.binary)
+
+        if args.cuckoo:
+            from cuckoo_wrapper import CuckooWrapper
+            print "Cuckoo analyzing", '.' * 30
+            analyzer = CuckooWrapper()
+            if args.binary != None:
+                response += str(analyzer.analyzeMalware(args.binary))
 
 
-    if args.jsunpackn:
-        from jsunpackn_wrapper import JsunpacknWrapper
-        print "Jsunpack-n analyzing", '.' * 30
-        analyzer = JsunpacknWrapper()
+        if args.jsunpackn:
+            from jsunpackn_wrapper import JsunpacknWrapper
+            print "Jsunpack-n analyzing", '.' * 30
+            analyzer = JsunpacknWrapper()
 
-        if args.url != None:
-            response += analyzer.analyzeJs(args.url)
+            if args.url != None:
+                response += analyzer.analyzeJs(args.url)
 
 if __name__ == "__main__":
     main()
