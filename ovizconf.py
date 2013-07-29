6db4ab5618c3c33f5605c413f85db8db4c7fcc67
ggercek <gurcangercek@gmail.com>
2013-07-29 12:53:21 +0300

    options for cuckoo. jsunpack and virustotal added/updated

--- ovizconf.py
+++ ovizconf.py
@@ -15,11 +15,16 @@ class Config():
 
         # Cuckoo options
         self.is_cuckoo_available = True
-        self.cuckoo_ip = "localhost"
-        self.cuckoo_port = 9000
+        self.cuckoo_ip = "81.167.148.242"
+        self.cuckoo_port = 8090
         self.cuckoo_html_enabled = True
         self.cuckoo_timeout = 60
         self.cuckoo_tcpdump_enabled = True
 
+        # Jsunpack-n options
+        self.jsunpackn_path = ""
+
+        # Virus Total API options
+        self.vt_apikey = ""
 
 
