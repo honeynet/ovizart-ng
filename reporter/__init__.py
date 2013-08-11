83f83f810ac09e9d76cd169bc05b123fe12b0050
mh <zqzas1@gmail.com>
2013-08-11 14:48:28 +0800

    html reporter

--- reporter/__init__.py
+++ reporter/__init__.py
@@ -1,5 +1,14 @@
 # Import inner modules
 
-__author__ = "ggercek"
+__author__ = "zqzas"
+
+class BaseReporter:
+    """Base Reporter"""
+
+    def __init__(self):
+        self.conf = None
+
+    def setConfig(self, config):
+        self.conf = config
 
 import html
--- reporter/html.py
+++ reporter/html.py
@@ -2,5 +2,97 @@
 HTML reporter
 """
 
-__author__ = "ggercek"
+__author__ = "zqzas"
+
+import sys
+sys.path.append("..")
+
+from core.engine import Reporter
+from core.tags import Tags
+from reporter import BaseReporter
+
+HTML = Tags.Reporter.HTML
+
+try:
+    from jinja2 import Environment, FileSystemLoader
+    HAVE_JINJA2 = True
+except ImportError:
+    HAVE_JINJA2 = False
+
+@Reporter(tags=HTML)
+class HTMLReporter(BaseReporter):
+    """Report HTML"""
+
+    def __init___(self):
+        BaseAnalyzer.__init__(self)
+
+    def __repr__(self):
+        return "HTML Reporter"
+
+    def report(self, data):
+        """
+        Generate HTML
+        @param results: results dict
+        
+        """
+
+        if not HAVE_JINJA2:
+            raise Exception("Failed to report HTML: Jinja2 Python Library is missing.")
+
+        #report path
+        results = data
+
+        if type(results) is str:
+            try:
+                results = json.loads(results)
+            except ValueError:
+                raise ValueError("No JSON object could be decoded; This method accepts json string or dict.")
+
+        env = Environment(autoescape=True, loader = FileSystemLoader('.'))
+
+        try:
+            tpl = env.get_template("report_template.html")
+            html = tpl.render({"results" : results})
+        except Exception as e:
+            raise Exception("HTML generation failed: %s" % e)
+
+        try:
+            report = open("report.html", "w")
+            report.write(html)
+            report.close()
+        except (TypeError, IOError) as e:
+            raise Exception("HTML write failed: %s" % e)
+
+
+
+"""
+if __name__ == "__main__":
+    rpt = HTMLReporter()
+    rpt.report({"hello": "zqzas", "13123":"131231231"})
+
+"""
+
+
+
+
+
+
+
+        
+
+
+
+        
+
+
+
+
+
+
+
+
+        
+
+
+
 
