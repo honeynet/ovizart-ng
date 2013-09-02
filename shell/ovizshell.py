1eaccb8e578d6ef3f99c881a97e943b7ce9bdc3d
mh <zqzas1@gmail.com>
2013-09-02 19:03:15 +0800

    try the cmd and readline module to automate command line

--- shell/ovizshell.py
+++ shell/ovizshell.py
@@ -4,72 +4,103 @@ import sys
 sys.path.append("..")
 
 from ovizart import Ovizart
-import ovizcli
+from ovizcli import cli_main
 
-'''
-import fysom
-
-fsm = Fysom({
-  'initial': 'initial',
-  'events': [
-    {'name': 'set_input',   'src': 'initial',   'dst': 'input_ok'},
-    {'name': 'set_tool',    'src': 'input_ok',  'dst': 'ready'},
-    {'name': 'set_tool',    'src': 'initial',   'dst': 'ready'},
-    {'name': 'analyze',     'src': 'ready',     'dst': 'analyzing'},
-    {'name': 'done',        'src': 'analyzing', 'dst': 'ready'},
-  ]
-})
-'''
-
-configData = {'output':'', 'input':'', 'external_tool': '', 'verbose':''}
-
-def getSet(cmd, set_dict):
-    set_pos = cmd.find('set ') + 4
-    equal_pos = cmd.find('=')
-    key = cmd[set_pos: equal_pos].lstrip().strip()
-    value = cmd[equal_pos + 1 :].split()[0]
-    set_dict[key] = value
-    return
-
-
-while True:
-    if configData['input'] == '':
-        print   """Please set input: a file or an url. \nExample:
-                set input=http://google.com
-                """
-
-    
-
-    print '>>>'
-
-    cmd = str(raw_input())
-
-    
-    if cmd.startswith('set'):
-        getSet(cmd, configData)
-    else:
-        if cmd.startswith('list-available'):
-            cli_args = ['-l']
+import cmd
+try:
+        import readline
+except ImportError:
+        try:
+                import pyreadline as readline
+        # throw open a browser if we fail both readline and pyreadline
+        except ImportError:
+                import webbrowser
+                webbrowser.open("http://ipython.scipy.org/moin/PyReadline/Intro#line-36")
+                # throw open a browser
+        #pass
+else:
+        import rlcompleter
+        if(sys.platform == 'darwin'):
+                readline.parse_and_bind ("bind ^I rl_complete")
         else:
-            if cmd.startswith('version'):
-                cli.args = ['-v']
-            else:
-                cli_args = []
-                
-                cli_args += ['-i', configData['input']]
-                if configData['output'] != '':
-                    cli_args += ['-o', configData['output']]
-                if configData['external_tool'] != '':
-                    cli_args += [configData['external_tool']]
-                if configData['verbose'] != '':
-                    cli_args += ['-V']
-        cli_main(cli_args)
+                readline.parse_and_bind("tab: complete")
+
+
+
+class OvizShell(cmd.Cmd):
+    SETOPTIONS= [ 'input', 'output', 'external_tool', 'verbose' ]
+
+    def __init__(self):
+        #super(OvizShell, self).__init__()
+        cmd.Cmd.__init__(self)
+        
+        #self.ovizcli_args = []
+        self.configData = {'output':'', 'input':'', 'external_tool': '', 'verbose':''}
+
+
+    def triggerOvizcli(self, args):
+        try:
+            cli_main(args)
+        except SystemExit:
+            print "Done."
         
 
 
-        
+    def getSet(self, cmd, set_dict):
+        set_pos = 0
+        equal_pos = cmd.find('=')
+        key = cmd[set_pos: equal_pos].lstrip().strip()
+        value = cmd[equal_pos + 1 :].split()[0]
+        set_dict[key] = value
+        return
+
+    def do_set(self, args):
+        "Set the config data."
+        self.getSet(args, self.configData)
+
+    def complete_set(self, text, line, begidx, endidx):
+        if not text:
+            completions = self.SETOPTIONS
+        else:
+            completions = [f for f in self.SETOPTIONS if f.startswith(text)]
+        return completions
+
+    def do_list(self, args):
+        "List the available modules."
+        self.triggerOvizcli(['-l'])
+
+    def do_version(self, args):
+        "Display the current version of Ovizart."
+        self.triggerOvizcli(['-v'])
+
+    def do_start(self, args):
+        "Start analyze."
+
+        cli_args = []
+        if self.configData['input'] != '':
+            cli_args += ['-i', self.configData['input']]
+        if self.configData['output'] != '':
+            cli_args += ['-o', self.configData['output']]
+        if self.configData['external_tool'] != '':
+            cli_args += [self.configData['external_tool']]
+        if self.configData['verbose'] != '':
+            cli_args += ['-V'] 
+        self.triggerOvizcli(cli_args)
+
+    def do_show(self, args):
+        "Show the state of config data at this moment."
+        print self.configData
+
+    def do_reset(self, args):
+        "Reset the config data such as input, output, etc."
+        self.configData = {'output':'', 'input':'', 'external_tool': '', 'verbose':''}
 
 
-        
+    def do_EOF(self, line):
+        "Stop the program."
+        return True
 
+if __name__ == '__main__':
+    OvizShell().cmdloop()
 
+ 
